5776940dbf5b674c46b076364e1c7e58986accda
ngodang020327-prog <ngodang020327@gmail.com>
2025-12-07 23:36:05 +0700

    Add basic C++ program to calculate sum of two integers

---
+++ b1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Khai báo 2 số nguyên
+    int a = 50;
+    int b = 100;
+
+    // Tính tổng
+    int total = a + b;
+
+    // In kết quả
+    cout << "Tong cua hai so la: " << total << endl;
+
+    return 0;
+}
