bb240713d616314d7480088de5d1ddaad397c405
ngodang020327-prog <ngodang020327@gmail.com>
2025-12-07 23:37:30 +0700

    Add program to calculate miles per gallon

---
+++ 10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double miles = 375.0;
+    double gallons = 15.0;
+
+    double mpg = miles / gallons;
+
+    cout << "So dam tren moi gallon: " << mpg << endl;
+
+    return 0;
+}
