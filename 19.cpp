f31f2c25cb301e9a03de924fc1a19963552f2341
ngodang020327-prog <ngodang020327@gmail.com>
2025-12-07 23:44:23 +0700

    Create 19.cpp

---
+++ 19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double ny = 85, den = 88, phx = 106;
+
+    cout << "Nhiet do moi New York: " << ny * 1.02 << endl;
+    cout << "Nhiet do moi Denver: " << den * 1.02 << endl;
+    cout << "Nhiet do moi Phoenix: " << phx * 1.02 << endl;
+
+    return 0;
+}
