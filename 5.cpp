aaa5c87c980aaf9882fac1a215fe408c4689e771
ngodang020327-prog <ngodang020327@gmail.com>
2025-12-07 23:47:59 +0700

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 28, b = 32, c = 37, d = 24, e = 33;
+
+    double sum = a + b + c + d + e;
+    double average = sum / 5;
+
+    cout << "Gia tri trung binh: " << average << endl;
+
+    return 0;
+}
