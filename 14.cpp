52c7172a22202e8d1afb4bc18879d1f7d8f40efb
ngodang020327-prog <ngodang020327@gmail.com>
2025-12-07 23:39:54 +0700

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Ho va ten: Dao Manh Cuong\nDia chi: Xuan Van, TP Tuyen Quang, ZIP 12345\nSo dien thoai: 0911056591\nChuyen nganh: CNTT";
+
+    return 0;
+}
